06c606040748b203e4d645aa65be47e282295e05
paulin-mipt <paulin.mipt@gmail.com>
2016-12-05 03:48:24 +0300

    gallery added

--- generator/urls.py
+++ generator/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='generator_index'),
+    url(r'^gallery$', views.gallery, name='gallery'),
 ]
--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .perashki_generator import make_random_perashok
+from datetime import datetime
 
 from .models import Perashok
 
@@ -8,9 +9,20 @@ def index(request):
     if "more" in request.POST:
         generated_text = make_random_perashok()
         context['perashok_lines'] = generated_text.split('\n')
+        context['perashok_text'] = generated_text
+    elif "save" in request.POST:
+        text = request.POST['hidden_text']
+        
+        perashok = Perashok(perashok_text=text, adding_date=datetime.now())
+        perashok.save()
+        
+        context['perashok_lines'] = text.split('\n')
+        context['perashok_text'] = text
+        context['info_message'] = "Трям! Пирожок успешно добавлен в галерею, потомки вас не забудут"
         
     return render(request, 'index.html', context)
 
-def perashki_list(request):
-    context = []
+def gallery(request):
+    perashki = [[p.perashok_text.split('\n'), p.adding_date] for p in Perashok.objects.all()]
+    context = {"perashki_list": perashki}
     return render(request, 'gallery.html', context)
